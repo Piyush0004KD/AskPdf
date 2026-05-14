bebb511f39304ed5b8e2cf729b031a069825b367
PiyushKD <piyushkd15@gmail.com>
2026-05-15 01:28:50 +0530

    added the rag folder

---
+++ rag/loader.py
@@ -0,0 +1,57 @@
+from pypdf import PdfReader
+from docx import Document
+import io
+
+
+
+def load(file_bytes: bytes , filename : str) -> str:
+    if filename.endswith(".pdf"):
+        text = load_pdf(file_bytes)
+
+    elif filename.endswith(".docx"):
+        text = load_docx(file_bytes)
+
+    elif filename.endswith(".txt"):
+        text = load_txt(file_bytes)
+
+    else:
+        raise ValueError(f"unsupported fileType : {filename}")
+
+    if not text.strip():
+        raise ValueError(f"No extractable text found in {filename} , file may be image based empty")
+
+    return text
+
+
+def load_pdf(file_bytes:bytes)-> str:
+    reader = PdfReader(io.BytesIO(file_bytes))
+    text=""
+    for page in reader.pages:
+        text += page.extract_text() or ""
+
+    return text
+
+
+def load_docx(file_bytes : bytes)-> str:
+    doc = Document(io.BytesIO(file_bytes))
+    text=""
+    for para in doc.paragraphs:
+        text += para.text + "\n"
+
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                text += cell.text + " "
+            text +="\n"
+
+    return text
+
+def load_txt(file_bytes : bytes)-> str:
+    try :
+        return file_bytes.decode("utf-8")
+    except UnicodeDecodeError:
+        return file_bytes.decode("latin-1")
+
+
+
+
